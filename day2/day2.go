88faa9c7347d206b45be9b5f89d6c0e82ea16d73
William Thomas <william.thomas@fourwindsinteractive.com>
2019-12-06 17:09:46 -0700

    completed day2, probably needs refactoring

--- day2/day2.go
+++ day2/day2.go
@@ -3,23 +3,37 @@ package main
 import (
 	inputreader "adventOfCode2019/datautils"
 	"fmt"
+	"math/rand"
 )
 
 const stepForward = 4
+const breakCode = 99
+const expectedOutput = 19690720
 
 func main() {
 	numbers := inputreader.Readfile("../data/day02a.txt", ",")
-	numbers[1] = 12
-	numbers[2] = 2
-
-	fmt.Println("answer", calculateOpCode(numbers, 1, 12, 2, 2))
+	fmt.Println("answer", calculateNounAndVerb(numbers))
 }
 
-func calculateOpCode(numbers []int, modifierLoc1 int, modifierNum1 int, modifierLoc2 int, modifierNum2 int) int {
-	numbers[modifierLoc1] = modifierNum1
-	numbers[modifierLoc2] = modifierNum2
+func calculateNounAndVerb(numbers []int) int {
+	intResult := 0
+	noun := 0
+	verb := 0
+	for intResult != expectedOutput {
+		noun = rand.Intn(99)
+		verb = rand.Intn(99)
+		input := []int{}
+		input = append(input, numbers...)
+		intResult = intCodeComputer(input, noun, verb)
+	}
+	return 100*noun + verb
+}
+
+func intCodeComputer(numbers []int, noun, verb int) int {
+	numbers[1] = noun
+	numbers[2] = verb
 	for i := 0; i < len(numbers); i += stepForward {
-		if numbers[i] == 99 {
+		if numbers[i] == breakCode {
 			break
 		}
 		opCode, pos1, pos2, resultPointer := numbers[i], numbers[i+1], numbers[i+2], numbers[i+3]
