d74e53d4bdb73d1a60f924a305ee05c5e3ab6d75
William Thomas <william.thomas@fourwindsinteractive.com>
2019-12-09 09:54:07 -0700

    day3, got collisions to show on grid, now need to calculate distance from start

--- day3/day3.go
+++ day3/day3.go
@@ -25,69 +25,77 @@ func NewWireGrid() WireGrid {
 	return wg
 }
 
-func (wg WireGrid) calculateAndAddPoints(instructions string, lastPosition Point) Point {
+func (wg WireGrid) displayMap() {
+	fmt.Println(wg)
+}
+
+func (wg WireGrid) calculateAndAddPoints(instructions string) {
 	// this needs to return the lastPosition, in order to receive it in next instruction
 	// need to create rules for all directions
 	// if already in map, increase count
 	// we will then look at records in map for occurances of 2, create function to calculate closest collision from occurances of 2
 	instructionsSplit := strings.Split(instructions, ",")
-	firstInstruction := string(instructionsSplit[0])
-	direction := string(firstInstruction[0])
-	distance, _ := strconv.Atoi(string(firstInstruction[1:]))
-	var finalPosition Point
-	if direction == "R" {
-		for i := 1; i <= distance; i++ {
-			newDistance := lastPosition.y + i
-			newPoint := Point{lastPosition.x, newDistance}
-			wg[newPoint] = 1
-			if i == distance {
-				finalPosition = newPoint
+	lastPosition := Point{0, 0}
+	for _, instruction := range instructionsSplit {
+		direction := string(instruction[0])
+		distance, _ := strconv.Atoi(string(instruction[1:]))
+		if direction == "R" {
+			for i := 1; i <= distance; i++ {
+				newDistance := lastPosition.y + i
+				newPoint := Point{lastPosition.x, newDistance}
+				if _, ok := wg[newPoint]; ok {
+					wg[newPoint]++
+					fmt.Println("found a collision", wg[newPoint])
+				} else {
+					wg[newPoint] = 1
+				}
+				if i == distance {
+					lastPosition = newPoint
+				}
 			}
 		}
-	}
-	if direction == "L" {
-		for i := 1; i <= distance; i++ {
-			newDistance := lastPosition.y - i
-			newPoint := Point{lastPosition.x, newDistance}
-			wg[newPoint] = 1
-			if i == distance {
-				finalPosition = newPoint
+		if direction == "L" {
+			for i := 1; i <= distance; i++ {
+				newDistance := lastPosition.y - i
+				newPoint := Point{lastPosition.x, newDistance}
+				wg[newPoint] = 1
+				if i == distance {
+					lastPosition = newPoint
+				}
 			}
 		}
-	}
 
-	if direction == "U" {
-		for i := 1; i <= distance; i++ {
-			newDistance := lastPosition.x + i
-			newPoint := Point{newDistance, lastPosition.y}
-			wg[newPoint] = 1
-			if i == distance {
-				finalPosition = newPoint
+		if direction == "U" {
+			for i := 1; i <= distance; i++ {
+				newDistance := lastPosition.x + i
+				newPoint := Point{newDistance, lastPosition.y}
+				wg[newPoint] = 1
+				if i == distance {
+					lastPosition = newPoint
+				}
+			}
+		}
+		if direction == "D" {
+			for i := 1; i <= distance; i++ {
+				newDistance := lastPosition.x - i
+				newPoint := Point{newDistance, lastPosition.y}
+				wg[newPoint] = 1
+				if i == distance {
+					lastPosition = newPoint
+				}
 			}
 		}
 	}
-	if direction == "D" {
-		for i := 1; i <= distance; i++ {
-			newDistance := lastPosition.x - i
-			newPoint := Point{newDistance, lastPosition.y}
-			wg[newPoint] = 1
-			if i == distance {
-				finalPosition = newPoint
-			}
-		}
-	}
-	return finalPosition
 }
 
 func main() {
 	instructions := ReadAndSeparateWireInstructions()
 	firstWire := instructions[0]
-	fmt.Println("firstWire", firstWire)
-	// secondWire := instructions[1]
+	secondWire := instructions[1]
 	wireGrid := NewWireGrid()
-	firstInstruction := firstWire[0]
-	fmt.Println("firstInstruction", firstInstruction)
-	wireGrid.calculateAndAddPoints(firstWire[0], Point{0, 0})
+	wireGrid.calculateAndAddPoints(firstWire[0])
+	wireGrid.calculateAndAddPoints(secondWire[0])
+	wireGrid.displayMap()
 }
 
 // ReadAndSeparateWireInstructions specific function for this challenge, splits into two lists of instructions
